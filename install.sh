a1fd748c57c97e3c92bfd9f6e5854ed3414ca063
Vosjedev <100359465+Vosjedev@users.noreply.github.com>
2022-12-02 13:46:54 +0000

    installer 1

---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/bash
+
+cd "$(dirname "$0")"
+src="$(pwd)"
+
+echo -n "loading... "
+function err {
+    trap "trap.err" ERR
+}
+function unerr {
+    trap "trap.ncerr" ERR
+}
+function trap.err {
+    echo -e "\nOOPS! something went wrong. cleaning up..."
+    rm -rf "$HOME/.vosjedev/packager"
+    exit 1
+} 
+function trap.ncerr {
+    echo "error!"
+}
+trap "trap.err" SIGINT
+
+git pull
+
+while :
+do
+[ "$HOME" > /dev/null ] || read -p "enter your home directory:" -e HOME
+[ "$HOME" > /dev/null ] && break
+done
+echo "done"
+
+err
+echo -n "making filesystem... "
+cd "$HOME"
+[ -d .vosjedev ] || mkdir .vosjedev
+cd .vosjedev
+dest="$(pwd)/packager"
+
+echo "done."
+cd "$src"
+echo "copying files..."
+cp -r ./ "$dest"
+echo "done."
--- run.sh
+++ run.sh
@@ -1,1 +1,46 @@
 #!/usr/bin/bash
+
+# set $COLUMNS if unset
+if ! [ "$COLUMNS" > /dev/null ]
+then COLUMNS="$(tput cols)"
+fi
+
+#       ___       __   _
+# \__\ \___ \    \__\ \/
+# \  \ \___ \___ \    0
+# function.
+function help {
+    echo "usage: $0 -[arguments] command query"
+    if [ "$COLUMNS" -ge 70 ]
+    then
+        echo "
+.-------------------------------------------------------------------.
+| commands                      : discription                       |
+|-------------------------------------------------------------------|
+| -h --help                     : show this help                    |
+
+"
+    else
+        echo "
+command : description
+h help : show this help
+"
+    fi
+    exit $1
+}
+
+# test if there is any argument
+[ "$@" == '' ] && {
+    help 1
+}
+
+# parse arguments
+while [[ "$#" -gt 0 ]]; do
+  case "${1:-}" in
+    -h | --help ) help ;;
+    * ) echo "did not reconise option '$1'."
+        echo "use \"$0\" -h for more info"
+        exit 1
+  esac
+  shift
+done
